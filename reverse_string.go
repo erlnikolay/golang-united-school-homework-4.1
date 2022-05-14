61d3282fe30279e752d6d1cc338579d113da0c18
erlnikolay <erl.nikolay@gmail.com>
2022-05-14 18:06:52 +0700

    Solution 1

--- reverse_string.go
+++ reverse_string.go
@@ -2,5 +2,17 @@ package reverse_string
 
 func ReverseString(input string) (output string) {
 	// solution goes here
+	sourceRuneString := []rune(input)
+	//fmt.Println(sourceRuneString)
+	resRuneString := make([]rune, len(sourceRuneString), cap(sourceRuneString))
+	for iter := len(sourceRuneString) - 1; iter >= 0; iter-- {
+		//fmt.Println(iter)
+		resRuneString[(len(sourceRuneString)-1)-iter] = sourceRuneString[iter]
+	}
+	output = string(resRuneString)
 	return output
 }
+
+//func main() {
+//	fmt.Println(ReverseString("А роза упала на лапу азорА"))
+//}
